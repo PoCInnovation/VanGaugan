596d39ac3737ca6ff50ec5f341e61b7fabf24b37
Clément Doucy <clement.doucy@epitech.eu>
2020-07-23 17:42:28 +0200

    feat(front): add basic UI

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,58 @@
 import React from 'react';
+
+import {
+    Grid,
+    TextField,
+    Select,
+    MenuItem,
+    InputLabel,
+    FormControl
+} from "@material-ui/core";
+
 import './App.css';
 
 function App() {
+    const [label, setLabel] = React.useState('');
+    const [imageNumber, setImageNumber] = React.useState(1);
+    const [selectValue, setSelectValue] = React.useState(1);
+
+    const handleChange = (event) => {
+        event.preventDefault();
+        let {value} = event.target;
+
+        setSelectValue(value);
+
+        if (value !== "" && value > 0 && value <= 64)
+            setImageNumber(value)
+    }
+
   return (
     <div className="App">
-        <img src="/api/celeba" alt="generated image"></img>
+        <Grid container direction="column" justify="space-between" alignItems="center" spacing={5}>
+            <Grid item>
+                <h1>VanGaugan</h1>
+            </Grid>
+            <Grid item>
+                <img
+                    src={`/api/celeba?image_number=${imageNumber}&label=${label}`}
+                    alt="generated image">
+                </img>
+            </Grid>
+            <Grid item>
+                <FormControl style={{maxWidth: 125}}>
+                    <TextField label="Image number" type="number" value={selectValue} onChange={handleChange}></TextField>
+                </FormControl>
+            </Grid>
+            <Grid item>
+                <FormControl style={{minWidth: 125}}>
+                    <InputLabel id="input-label">Label</InputLabel>
+                    <Select id="input-label" value={label} onChange={(ev) => setLabel(ev.target.value)}>
+                        <MenuItem value={"toto"}>toto</MenuItem>
+                        <MenuItem value={"tata"}>tata</MenuItem>
+                    </Select>
+                </FormControl>
+            </Grid>
+        </Grid>
     </div>
   );
 }
